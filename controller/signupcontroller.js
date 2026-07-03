70004abdf48fa2901b3951b48b381b80986aa789
ManishYadav23466 <yadavmanish23466@gmail.com>
2026-07-03 16:04:15 +0530

    changes in cookies

--- controller/signupcontroller.js
+++ controller/signupcontroller.js
@@ -79,8 +79,8 @@ const signin=async (req,res)=>{
 
         res.cookie("token",token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "lax",
+            secure: true,
+            sameSite: "none",
             maxAge: 24 * 60 * 60 * 1000
         });
 
